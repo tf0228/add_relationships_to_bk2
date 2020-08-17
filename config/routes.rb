c7fefc750f848b729a9d4ec3cfc289c2fed887e4
vagrant <vagrant@localhost.localdomain>
2020-08-17 07:11:00 +0000

    [Start] reletionships

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'home#top'
+  get 'home/about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+end
